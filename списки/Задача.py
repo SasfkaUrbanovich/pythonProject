8ea43d59a2973926841bce0a1a76a7e0102c3851
Sasfka <sasfka@icloud.com>
2021-11-11 15:50:00 +0300

    add "exponentiation" by Sasfka

--- списки/Задача.py
+++ списки/Задача.py
@@ -1,1 +1,9 @@
-print("Начало")
+def exponentiation():
+    exponen = 1
+    number = int(input("Введите число "))
+    for _ in range(int(input("Введите степень "))):
+        print(_)
+        exponen = exponen * number
+    print(exponen)
+
+exponentiation()
