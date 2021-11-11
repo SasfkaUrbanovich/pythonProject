98e6101e0686f2923024a03c667eef12025d72e2
Sasfka <sasfka@icloud.com>
2021-11-11 15:15:38 +0300

    add "min" by Sasfka

--- списки/Задача.py
+++ списки/Задача.py
@@ -1,1 +1,10 @@
-print("Начало")
+array = []
+def miin():
+    for _ in range(3):
+        array.append(int(input('Введите число ')))
+    mini = array[0]
+    for item in array:
+        if mini > item :
+            mini = item
+    print(mini)
+miin()
