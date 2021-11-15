5058407588a361e693a430f87dfbe9722b6acdc9
SasfkaUrbanovich <sasfka@icloud.com>
2021-11-15 19:05:04 +0300

    rebuild

--- списки/task2.py
+++ списки/task2.py
@@ -1,10 +1,13 @@
-array = []
+import random
+
 def miin():
-    for _ in range(3):
-        array.append(int(input('Введите число ')))
+    array = [random.randint(1, 50) for x in range(3)]
     mini = array[0]
     for item in array:
         if mini > item :
             mini = item
     print(mini)
-miin()
+
+
+if __name__ == '__main__':
+    miin()
--- списки/task3.py
+++ списки/task3.py
@@ -2,7 +2,6 @@ def exponentiation():
     exponen = 1
     number = int(input("Введите число "))
     for _ in range(int(input("Введите степень "))):
-        print(_)
         exponen = exponen * number
     print(exponen)
 
